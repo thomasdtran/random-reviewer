64996b2cb48ce5cdb454ca6ca5054aec2d99f5ce
thomasdtran <61562361+thomasdtran@users.noreply.github.com>
2024-10-16 12:43:32 +0200

    Fix input for reviewers to string

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try{
-    const reviewers = core.getInput('reviewers')
+    const reviewers = core.getInput('reviewers').json
 
     core.setOutput('assigned', reviewers[0])
-    
+
 }catch(error){
     core.setFailed(error.message)
 }
